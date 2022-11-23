e5fd0d940541720e7920d2056613d7b0efa528f4
Dmitriy Dzhurabaev <blood.raven.707@gmail.com>
2022-11-24 02:32:03 +0300

    Initial commit

---
+++ Final_Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Console_Program
+{
+    class Project
+    {
+        private static void Main( string[] args ) {
+            Console.WriteLine( "\nИтоговая проверочная работа v1\n" );
+        }
+    }
+}
